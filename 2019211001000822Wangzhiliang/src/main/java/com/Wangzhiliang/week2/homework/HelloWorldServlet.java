570a8da277f2a29c52dc3ab9727e298330bf7ea3
王志良 <61832938+jswzl@users.noreply.github.com>
2021-03-12 15:47:43 +0800

    Add files via upload
    HW-CE1 and HW-CE2 commit

---
+++ 2019211001000822Wangzhiliang/src/main/java/com/Wangzhiliang/week2/homework/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package com.Wangzhiliang.week2.homework;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        java.util.Date today = new java.util.Date();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1 align=center>"+"Name:Wang zhiliang"+"</h1>");
+        out.println("<h1 align=center>"+"ID:2019211001000822"+"</h1>");
+        out.println("<h1 align=center>"+"Date:"+today+"</h1>");
+        out.println("</body>");
+        out.println("<html>");
+    }
+}
