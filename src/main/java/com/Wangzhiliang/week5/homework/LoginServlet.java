c918a8a5fffca7bb474d48e0c9f2bf2366aaba20
王志良 <61832938+jswzl@users.noreply.github.com>
2021-04-26 21:30:27 +0800

    8.HW-week8

--- src/main/java/com/Wangzhiliang/week5/homework/LoginServlet.java
+++ src/main/java/com/Wangzhiliang/week5/homework/LoginServlet.java
@@ -1,19 +1,14 @@
-package com.ZhouZhiWei.week5;
+package com.Wangzhiliang.week5.homework;
 
-import com.ZhouZhiWei.dao.UserDao;
-import com.ZhouZhiWei.model.User;
+import com.Wangzhiliang.dao.UserDao;
+import com.Wangzhiliang.model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 @WebServlet(name = "LoginServlet",value = "/login")
 public class LoginServlet extends HttpServlet {
@@ -21,7 +16,16 @@ public class LoginServlet extends HttpServlet {
     Connection con = null;
     @Override
     public void init() throws ServletException {
-
+//        String url = getServletConfig().getServletContext().getInitParameter("url");
+//        String driver = getServletConfig().getServletContext().getInitParameter("driver");
+//        String username = getServletConfig().getServletContext().getInitParameter("username");
+//        String password = getServletConfig().getServletContext().getInitParameter("password");
+//        try {
+//            Class.forName(driver);
+//            con = DriverManager.getConnection(url,username,password);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         con = (Connection) getServletContext().getAttribute("con");
     }
 
@@ -34,7 +38,25 @@ public class LoginServlet extends HttpServlet {
         try {
             User user= userDao.findByUsernamePassword(con,username,password);
             if(user!=null) {
-                request.setAttribute("user",user);
+
+                String rememberMe=request.getParameter("rememberMe");
+                if(rememberMe!=null && rememberMe.equals("1")){
+                    Cookie usernameCookie=new Cookie("cUsername",user.getUsername());
+                    Cookie passwordCookie=new Cookie("cPassword",user.getPassword());
+                    Cookie rememberMeCookie=new Cookie("cRememberMe",rememberMe);
+
+                    usernameCookie.setMaxAge(5);
+                    passwordCookie.setMaxAge(5);
+                    rememberMeCookie.setMaxAge(5);
+
+                    response.addCookie(usernameCookie);
+                    response.addCookie(passwordCookie);
+                    response.addCookie(rememberMeCookie);
+                }
+                HttpSession session = request.getSession();
+                System.out.println("Session id" + session.getId());
+                session.setAttribute("user",user);
+                //request.setAttribute("user",user);
                 request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
             }
             else {
@@ -46,10 +68,39 @@ public class LoginServlet extends HttpServlet {
             throwables.printStackTrace();
         }
 
+
+//        try {
+//            String sql="select * from usertab where username=? and password=?";
+//            PreparedStatement ps=con.prepareStatement(sql);
+//            ps.setString(1,username);
+//            ps.setString(2,password);
+//            ResultSet rs=ps.executeQuery();
+//            if(rs.next()) {
+////                out.println("Login Success!!!");
+////                out.println("Welcome, "+username);
+//                request.setAttribute("id",rs.getInt("id"));
+//                request.setAttribute("username",rs.getString("username"));
+//                request.setAttribute("password",rs.getString("password"));
+//                request.setAttribute("email",rs.getString("email"));
+//                request.setAttribute("gender",rs.getString("gender"));
+//                request.setAttribute("birthday",rs.getString("birthday"));
+//                request.getRequestDispatcher("userInfo.jsp").forward(request,response);
+//            }
+//            else{
+//                request.setAttribute("message","Username or Password Error!!!");
+//                request.getRequestDispatcher("login.jsp").forward(request,response);
+//
+//            }
+//
+////                out.println("Username or Password Error!!!");
+//
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
     }
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+        doPost(request, response);
     }
 }
