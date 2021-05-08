4f922c6f8b77a7aa54282fbbb2d1ee3906642435
王志良 <61832938+jswzl@users.noreply.github.com>
2021-05-08 09:39:33 +0800

    9.HW-week9

--- src/main/java/com/Wangzhiliang/controller/LogoutServlet.java
+++ src/main/java/com/Wangzhiliang/controller/LogoutServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @WebServlet(name = "LogoutServlet",value = "/logout")
 public class LogoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
