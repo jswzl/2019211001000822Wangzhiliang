d0a0f1d70a1df4d00524b5b361eebfea931bf45d
王志良 <61832938+jswzl@users.noreply.github.com>
2021-05-19 16:55:48 +0800

    12.HW-week12

--- src/main/java/com/Wangzhiliang/controller/ProductListServlet.java
+++ src/main/java/com/Wangzhiliang/controller/ProductListServlet.java
@@ -1,19 +1,39 @@
 package com.Wangzhiliang.controller;
 
+import com.Wangzhiliang.dao.ProductDao;
+import com.Wangzhiliang.model.Product;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "ProductListServlet",value = "/admin/productList")
 public class ProductListServlet extends HttpServlet {
+
+    Connection con = null;
+    @Override
+    public void init() throws ServletException {
+
+        con = (Connection) getServletContext().getAttribute("con");
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            ProductDao productDao = new ProductDao();
+            List<Product> productList = productDao.findAll(con);
+            request.setAttribute("productList",productList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         request.getRequestDispatcher("/WEB-INF/views/admin/productList.jsp").forward(request,response);
     }
 }
