687b94b85601a817a441afc8a94e37f0611dde71
王志良 <61832938+jswzl@users.noreply.github.com>
2021-05-30 11:50:35 +0800

    13.HW-week13

---
+++ src/main/java/com/Wangzhiliang/controller/UserListServlet.java
@@ -0,0 +1,32 @@
+package com.Wangzhiliang.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet("/admin/userList")
+public class UserListServlet extends HttpServlet {
+    Connection con=null;
+
+    @Override
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/admin/userList.jsp").forward(request,response);
+    }
+}
